670f7313003f69141bfdf113358933a61a95a856
arjun bollam <arjun bollam>
2017-02-02 22:13:57 +0530

    Added display method

---
+++ src/GitMerge.java
@@ -0,0 +1,18 @@
+
+public class GitMerge {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+         GitMerge gt = new GitMerge();
+         gt.Display();
+	}
+	
+	static void Display()
+	{
+		System.out.println("Arjun");
+	}
+
+}
